b809fbdad0db46846f5bdb05677598574c77de38
Ben Stovell <stovell94@gmail.com>
2019-01-05 19:05:13 +0000

    Content refactoring.

--- Source/JttU/JttU.Build.cs
+++ Source/JttU/JttU.Build.cs
@@ -8,7 +8,7 @@ public class JttU : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/JttU/Public/Character/JCharacter.h
+++ Source/JttU/Public/Character/JCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "JCharacter.generated.h"
 
+class UCameraComponent;
+class UDecalComponent;
+class USpringArmComponent;
+
 UCLASS(config=Game)
 class JTTU_API AJCharacter : public ACharacter
 {
@@ -14,51 +18,29 @@ class JTTU_API AJCharacter : public ACharacter
 public:
 	AJCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-	/** Camera boom positioning the camera behind the character */
+private:
+	/** Top down camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	UCameraComponent* TopDownCameraComponent;
 
-	/** Follow camera */
+	/** Camera boom positioning the camera above the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+	USpringArmComponent* CameraBoom;
+
+	/** A decal that projects to the cursor location. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UDecalComponent* CursorToWorld;
 
 public:
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
-
-protected:
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
-
-	/**
-	 * Called via input to turn at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
+	// Called every frame.
+	virtual void Tick(float DeltaSeconds) override;
 
 public:
+	/** Returns TopDownCameraComponent subobject **/
+	FORCEINLINE UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
 	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns CursorToWorld subobject **/
+	FORCEINLINE UDecalComponent* GetCursorToWorld() { return CursorToWorld; }
 
 };
